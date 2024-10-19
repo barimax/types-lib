4d7da001c36c05a04f9c2da4f76b8429cf8f6505
Georgie Ivanov <georgie@barimax.bg>
2024-10-19 17:26:10 +0300

    Add migrations

--- Sources/TypesLib/Types-Lib.swift
+++ Sources/TypesLib/Types-Lib.swift
@@ -5,6 +5,10 @@ import Fluent
 public final class TypesLib {
     public static func migrateTypesLib(app: inout Application){
         app.migrations.add(Company.Migration())
+        app.migrations.add(User.Migration())
+        app.migrations.add(UserCompanyRelation.Migration())
+        app.migrations.add(Token.Migration())
+        app.migrations.add(Accountant.Migration())
     }
 }
 
