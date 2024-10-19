93b1b80d80e770335174a357ae71cee908778101
Georgie Ivanov <georgie@barimax.bg>
2024-10-19 17:30:37 +0300

    addTypesLibMigrations()

--- Sources/TypesLib/Types-Lib.swift
+++ Sources/TypesLib/Types-Lib.swift
@@ -1,14 +1,12 @@
 import Vapor
 import Fluent
 
-
-public final class TypesLib {
-    public static func migrateTypesLib(app: inout Application){
-        app.migrations.add(Company.Migration())
-        app.migrations.add(User.Migration())
-        app.migrations.add(UserCompanyRelation.Migration())
-        app.migrations.add(Token.Migration())
-        app.migrations.add(Accountant.Migration())
+public extension Application {
+    func addTypesLibMigrations() {
+        self.migrations.add(Company.Migration())
+        self.migrations.add(User.Migration())
+        self.migrations.add(UserCompanyRelation.Migration())
+        self.migrations.add(Token.Migration())
+        self.migrations.add(Accountant.Migration())
     }
 }
-
