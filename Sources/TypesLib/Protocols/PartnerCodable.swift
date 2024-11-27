b49612f30eb6b59b8b00a9571c21e6caad0a171d
Georgie Ivanov <georgie@barimax.bg>
2024-11-27 13:49:16 +0200

    Make protocol public

--- Sources/TypesLib/Protocols/PartnerCodable.swift
+++ Sources/TypesLib/Protocols/PartnerCodable.swift
@@ -5,9 +5,9 @@
 //  Created by Georgie Ivanov on 27.11.24.
 //
 
-protocol PartnerCodable: Codable {
+public protocol PartnerCodable: Codable {
     var name: String { get set }
     var uid: String? { get set }
     var address: String? { get set }
-    var configuration: 
+    var configuration: PartnerConfiguration? { get set }
 }
---
+++ Sources/TypesLib/Structures/PartnerConfiguration.swift
@@ -0,0 +1,16 @@
+//
+//  PartnerConfiguration.swift
+//  types-lib
+//
+//  Created by Georgie Ivanov on 27.11.24.
+//
+
+public struct PartnerConfiguration: Codable {
+    public var bankAccounts: [BankAccount]?
+    
+    public init(
+        bankAccounts: [BankAccount]? = nil
+    ) {
+        self.bankAccounts = bankAccounts
+    }
+}
