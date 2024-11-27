320448f98fa1c33ac01d6142b84196d4d8ef1ded
Georgie Ivanov <georgie@barimax.bg>
2024-11-27 13:46:48 +0200

    Add PartnerCodable protocol

---
+++ Sources/TypesLib/Protocols/PartnerCodable.swift
@@ -0,0 +1,13 @@
+//
+//  Partner.swift
+//  types-lib
+//
+//  Created by Georgie Ivanov on 27.11.24.
+//
+
+protocol PartnerCodable: Codable {
+    var name: String { get set }
+    var uid: String? { get set }
+    var address: String? { get set }
+    var configuration: 
+}
