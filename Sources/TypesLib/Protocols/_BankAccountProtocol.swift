8d4d176e13ad8e5e7814c89fd1e56db405120bdc
Georgie Ivanov <georgie@barimax.bg>
2024-12-05 14:59:00 +0200

    Refactor BankAccount and Partner configuration

---
+++ Sources/TypesLib/Protocols/_BankAccountProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  BankAccountProtocol.swift
+//  types-lib
+//
+//  Created by Georgie Ivanov on 27.11.24.
+//
+
+//public protocol BankAccountProtocol: Codable, Sendable {
+//    var bic: String? { get set }
+//    var iban: String { get set }
+//    var name: String { get set }
+//    var currency: Currency { get set }
+//}
---
+++ Sources/TypesLib/Structures/BankAccount.swift
@@ -0,0 +1,111 @@
+//
+//  BankAccount.swift
+//  types-lib
+//
+//  Created by Georgie Ivanov on 5.12.24.
+//
+
+import Vapor
+import Fluent
+
+public final class BankAccount: Codable, Sendable {
+    
+    public let bic: String
+    public let name: String
+    public let iban: String
+    public let currency: Currency
+    public let bankAccount: Int
+    public let clientsAccount: Int?
+    public let suppliersAccount: Int?
+    public let bankCostsAccount: Int?
+    public let bankCostsSearch: String?
+    public let cashAccount: Int?
+    public let cashSearch: String?
+    public let register: Int?
+    public let accountCriteria: [BankAccount.AccountCriteria]?
+    public let accountDetails: BankAccount.AccountDetails?
+    
+    public init(bic: String, name: String, iban: String, currency: Currency, bankAccount: Int, clientsAccount: Int?, suppliersAccount: Int?, bankCostsAccount: Int?, bankCostsSearch: String?, cashAccount: Int?, cashSearch: String?, register: Int?, accountCriteria: [BankAccount.AccountCriteria]?, accountDetails: BankAccount.AccountDetails?) {
+        self.bic = bic
+        self.name = name
+        self.iban = iban
+        self.currency = currency
+        self.bankAccount = bankAccount
+        self.clientsAccount = clientsAccount
+        self.suppliersAccount = suppliersAccount
+        self.bankCostsAccount = bankCostsAccount
+        self.bankCostsSearch = bankCostsSearch
+        self.cashAccount = cashAccount
+        self.cashSearch = cashSearch
+        self.register = register
+        self.accountCriteria = accountCriteria
+        self.accountDetails = accountDetails
+    }
+    
+    public struct Create: Content {
+        let iban: String
+        let currency: Currency
+        let bankAccount: Int
+        let clientsAccount: Int?
+        let suppliersAccount: Int?
+        let bankCostsAccount: Int?
+        let bankCostsSearch: String?
+        let cashAccount: Int?
+        let cashSearch: String?
+        let register: Int?
+        let accountCriteria: [BankAccount.AccountCriteria]?
+        let accountDetails: BankAccount.AccountDetails?
+    }
+    
+    public struct Update: Content {
+        let id: UUID
+        let iban: String
+        let currency: Currency
+        let bankAccount: Int
+        let clientsAccount: Int?
+        let suppliersAccount: Int?
+        let bankCostsAccount: Int?
+        let bankCostsSearch: String?
+        let cashAccount: Int?
+        let cashSearch: String?
+        let register: Int?
+        let accountCriteria: [BankAccount.AccountCriteria]?
+        let accountDetails: BankAccount.AccountDetails?
+    }
+    
+    public struct AccountCriteria: Codable, Sendable {
+        let account:  Int
+        let search: String?
+        let isPartner: Bool
+    }
+
+    public struct AccountDetails: Codable, Sendable {
+        let isSingleColumn: Bool
+        let isNegativeColumn: Bool
+        let singleColumn: String?
+        let additionalColumn: String?
+        let debitColumn: String?
+        let creditColumn: String?
+        let debitText: String?
+        let creditText: String?
+        let dateColumn: String?
+        let dateFormat: String?
+    }
+
+}
+extension BankAccount.Create: Validatable {
+    public static func validations(_ validations: inout Validations) {
+        validations.add("iban", as: String.self, is: !.empty)
+        validations.add("currency", as: Currency.self, is: .valid)
+        validations.add("bankAccount", as: Int.self, is: .valid)
+    }
+}
+
+extension BankAccount.Update: Validatable {
+    public static func validations(_ validations: inout Validations) {
+        validations.add("id", as: UUID.self, is: .valid)
+        validations.add("iban", as: String.self, is: !.empty)
+        validations.add("currency", as: Currency.self, is: .valid)
+        validations.add("bankAccount", as: Int.self, is: .valid)
+    }
+}
