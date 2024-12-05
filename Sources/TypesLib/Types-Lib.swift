303181dbbb2d83d31c126053ea5ca59280399525
Georgie Ivanov <georgie@barimax.bg>
2024-12-05 16:41:36 +0200

    JSON formatter

--- Sources/TypesLib/Types-Lib.swift
+++ Sources/TypesLib/Types-Lib.swift
@@ -1,6 +1,11 @@
 import Vapor
 import Fluent
 
+class Configuration {
+    static var encoder: JSONEncoder = JSONEncoder()
+    static var decoder: JSONDecoder = JSONDecoder()
+}
+
 public extension Application {
     func addTypesLibMigrations() {
         self.migrations.add(Company.Migration())
@@ -8,5 +13,20 @@ public extension Application {
         self.migrations.add(UserCompanyRelation.Migration())
         self.migrations.add(Token.Migration())
         self.migrations.add(Accountant.Migration())
+    }
+    
+    func encoderSetup() {
+        // JSON coding configuration
+        let decoder = JSONDecoder()
+        let encoder = JSONEncoder()
+        let formater = DateFormatter()
+        formater.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        formater.timeZone = TimeZone(abbreviation: "GMT")
+        decoder.dateDecodingStrategy = .formatted(formater)
+        encoder.dateEncodingStrategy = .formatted(formater)
+        Configuration.encoder = encoder
+        Configuration.decoder = decoder
+        ContentConfiguration.global.use(decoder: decoder, for: .json)
+        ContentConfiguration.global.use(encoder: encoder, for: .json)
     }
 }
