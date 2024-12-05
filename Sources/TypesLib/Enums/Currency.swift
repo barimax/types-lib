9b49103b36660f7a026d453b31897b9104e33782
Georgie Ivanov <georgie@barimax.bg>
2024-12-05 12:45:54 +0200

    Remove BankAccount

---
+++ Sources/TypesLib/Enums/Currency.swift
@@ -0,0 +1,7 @@
+//
+//  Curremcy.swift
+//  types-lib
+//
+//  Created by Georgie Ivanov on 4.12.24.
+//
+
--- Sources/TypesLib/Structures/_BankAccount.swift
+++ Sources/TypesLib/Structures/_BankAccount.swift
@@ -9,10 +9,12 @@ public struct BankAccount: BankAccountProtocol {
     public var name: String
     public var iban: String
     public var bic: String?
+    public var currency: Currency
     
-    public init(name: String, iban: String, bic: String?) {
+    public init(name: String, iban: String, bic: String?, currency: Currency) {
         self.name = name
         self.iban = iban
         self.bic = bic
+        self.currency = currency
     }
 }
