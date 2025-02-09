9a0c92c7367a50adc1d75108a049206f0ddb5957
Georgie Ivanov <georgie@barimax.bg>
2025-02-09 13:33:22 +0200

    Add some request ectensions

---
+++ Sources/TypesLib/Extensions/RequestExtension.swift
@@ -0,0 +1,62 @@
+//
+//  RequestExtension.swift
+//  types-lib
+//
+//  Created by Georgie Ivanov on 9.02.25.
+//
+
+import Vapor
+import Fluent
+
+public extension Request {
+    func user() async throws -> User {
+        guard let spiHost = self.headers.first(name: "X-spi-host"),
+              let userIDString = self.headers.first(name: "X-auth-user-id"),
+              let token = self.headers.bearerAuthorization?.token else {
+            throw Abort(.unauthorized)
+        }
+        
+        let userResponse = try await self.client.get(
+            "http://\(spiHost)/spi/user/get/\(userIDString)",
+            headers: [
+                "Authorization": "Bearer \(token)",
+                "Content-Type": "application/json",
+            ]
+        )
+        self.application.logger.debug("User: \(userResponse)")
+        let user = try userResponse.content.decode(User.self)
+        return user
+    }
+    
+    func listCompanies() async throws -> [Company] {
+        guard let spiHost = self.headers.first(name: "X-spi-host"),
+              let token = self.headers.bearerAuthorization?.token else {
+            throw Abort(.unauthorized)
+        }
+        
+        let userResponse = try await self.client.get(
+            "http://\(spiHost)/spi/company/get",
+            headers: [
+                "Authorization": "Bearer \(token)",
+                "Content-Type": "application/json",
+            ]
+            )
+        return try userResponse.content.decode([Company].self)
+    }
+    
+    func getCompany(id: UUID) async throws -> Company {
+        guard let spiHost = self.headers.first(name: "X-spi-host"),
+              let token = self.headers.bearerAuthorization?.token else {
+            throw Abort(.unauthorized)
+        }
+        
+        let userResponse = try await self.client.get(
+            "http://\(spiHost)/spi/company/get/\(id)",
+            headers: [
+                "Authorization": "Bearer \(token)",
+                "Content-Type": "application/json",
+            ]
+            )
+        return try userResponse.content.decode(Company.self)
+    }
+}
