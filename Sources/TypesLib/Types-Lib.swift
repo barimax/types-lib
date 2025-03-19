6e0b10064c9112aa4565bad91cad8d4d1ee17b3e
Georgie Ivanov <georgie@barimax.bg>
2025-03-19 11:01:45 +0200

    Add appConfiguration

--- Sources/TypesLib/Types-Lib.swift
+++ Sources/TypesLib/Types-Lib.swift
@@ -4,11 +4,31 @@ import MySQLKit
 import FluentMySQLDriver
 
 public class Configuration {
+    @available(*, deprecated)
     public static var encoder: JSONEncoder = JSONEncoder()
+    @available(*, deprecated)
     public static var decoder: JSONDecoder = JSONDecoder()
+    public var encoder: JSONEncoder
+    public var decoder: JSONDecoder
+    
+    public init(encoder: JSONEncoder = JSONEncoder(), decoder: JSONDecoder = JSONDecoder()) {
+        self.encoder = encoder
+        self.decoder = decoder
+    }
 }
 
 public extension Application {
+    struct AppConfigurationKey: StorageKey {
+        public typealias Value = Configuration
+    }
+    var appConfiguration: Configuration {
+        get {
+            self.storage[AppConfigurationKey.self] ?? Configuration()
+        }
+        set {
+            self.storage[AppConfigurationKey.self] = newValue
+        }
+    }
     
    
     func addTypesLibMigrations() {
@@ -32,6 +52,7 @@ public extension Application {
         Configuration.decoder = decoder
         ContentConfiguration.global.use(decoder: decoder, for: .json)
         ContentConfiguration.global.use(encoder: encoder, for: .json)
+        appConfiguration = Configuration(encoder: encoder, decoder: decoder)
     }
 }
 
