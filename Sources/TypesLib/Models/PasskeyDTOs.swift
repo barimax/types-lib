101080a4b21a696d873874fb0eddd9bdda567441
Georgie Ivanov <georgie@barimax.bg>
2025-12-08 19:42:36 +0200

    Add inits

--- Sources/TypesLib/Models/PasskeyDTOs.swift
+++ Sources/TypesLib/Models/PasskeyDTOs.swift
@@ -12,17 +12,34 @@ import Vapor
 // Wrapper so JSON matches what WebAuthn/Flutter expect: { "publicKey": { ... } }
 public struct CreationOptionsResponse: Content {
     public let publicKey: PublicKeyCredentialCreationOptions
+    
+    public init(publicKey: PublicKeyCredentialCreationOptions) {
+        self.publicKey = publicKey
+    }
 }
 
 public struct RequestOptionsResponse: Content {
     public let publicKey: PublicKeyCredentialRequestOptions
+    
+    public init(publicKey: PublicKeyCredentialRequestOptions) {
+        self.publicKey = publicKey
+    }
 }
 
 public struct StartRegisterRequest: Content {
     public let email: String
     public let displayName: String?
+    
+    public init(email: String, displayName: String?) {
+        self.email = email
+        self.displayName = displayName
+    }
 }
 
 public struct StartLoginRequest: Content {
     public let email: String
+    
+    public init(email: String) {
+        self.email = email
+    }
 }
